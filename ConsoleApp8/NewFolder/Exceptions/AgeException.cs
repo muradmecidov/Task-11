8c1167c8d80d27ec8c41ef3cf41ac46e9f360c36
muradmecidov <muradmecidov1499@gmail.com>
2023-04-20 11:05:59 +0400

    Task11

---
+++ ConsoleApp8/NewFolder/Exceptions/AgeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.NewFolder.Exceptions
+{
+    public class AgeException:Exception
+    {
+        public AgeException()
+        {
+        }
+
+        public AgeException(string message) : base(message) 
+        { 
+
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp8.NewFolder.Exceptions;
+using ConsoleApp8.NewFolder1;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+         
+
+            try
+            {
+                Human human = new Human();
+                human.Name = Console.ReadLine();
+                human.Surname = Console.ReadLine();
+                human.Age = int.Parse(Console.ReadLine());
+
+                if (human.Age == 0 || human.Age < 0)
+                {
+                    throw new AgeException("Yas yanlisdir");
+
+                }
+
+                
+
+            }
+            catch (AgeException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
